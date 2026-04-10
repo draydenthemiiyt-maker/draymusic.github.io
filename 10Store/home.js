3e0bd756ac39e4855aa9ea82d61f5d84ccadfda5
draydenthemiiyt-maker <draydenthemiiyt@gmail.com>
2026-04-10 15:26:20 +0100

    Update home.js

--- 10Store/home.js
+++ 10Store/home.js
@@ -4,9 +4,18 @@
     var featuredApps = [];
     var currentHeroIndex = 0;
     var heroTimer = null;
-    var ua = navigator.userAgent || navigator.vendor || window.opera;
-    var isMobile = /Mobi|Android|iPhone|iPad|Windows Phone|IEMobile/i.test(ua);
-    var isPC = !isMobile;
+    var deviceFamily = "Windows.Desktop";
+
+    try {
+        if (typeof Windows !== 'undefined') {
+            deviceFamily = Windows.System.Profile.AnalyticsInfo.versionInfo.deviceFamily;
+        }
+    } catch (e) {
+        console.warn("WinRT namespaces not found.");
+    }
+
+    var isMobile = (deviceFamily === "Windows.Mobile");
+    var isPC = (deviceFamily === "Windows.Desktop");
 
     function getVal(parent, tag) {
         var el = parent.getElementsByTagName(tag)[0];
